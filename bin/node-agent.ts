94d4225448a858e54d051a1c7c59f2a331eeb6b5
Stephan Hesse <tchakabam@gmail.com>
2018-09-20 03:25:20 +0200

    scripts and deps for node agent

---
+++ bin/node-agent.ts
@@ -0,0 +1,18 @@
+require('./node-shims');
+
+import {Engine, BKAccessProxyEvents} from '../engines/universal-engine/lib';
+
+const engine = new Engine();
+
+engine.getProxy().on(BKAccessProxyEvents.PeerConnect, () => {
+    console.log('peer connected');
+})
+
+engine.getPlayhead().on('update', () => {
+    console.log('time:', engine.getPlayhead().getCurrentTime());
+})
+
+engine.getPlayhead().play();
+
+engine.setSource("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8");
+engine.start();
---
+++ bin/node-shims.js
@@ -0,0 +1,21 @@
+// Standalone usage
+var XMLHttpRequest = require('node-http-xhr');
+
+// Usage as global XHR constructor
+global.XMLHttpRequest = require('node-http-xhr');
+
+var URL = require('url');
+
+global.URL = URL;
+
+global.window = {
+    performance: {
+        now: () => Date.now()
+    },
+    setTimeout: global.setTimeout,
+    setInterval: global.setInterval,
+    clearTimeout: global.clearTimeout,
+    clearInterval: global.clearInterval
+}
+
+global.wrtc = require('wrtc');
