fbd614227c93532e5dd6b9766ecba09e28fe17da
Stephan Hesse <tchakabam@gmail.com>
2018-09-26 15:06:07 +0200

    agent herd script

--- bin/agent-herd.ts
+++ bin/agent-herd.ts
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process');
-
 const argv = require('yargs').argv
 
-const WebSocketServer = require('websocket').server;
-const http = require('http');
-
-const WS_SERVER_PORT = 15555;
 const agentChildProcs = new Map();
 
 function forkNewAgentProc() {
@@ -64,60 +59,3 @@ if (typeof argv.instances === 'number') {
         forkNewAgentProc();
     }
 }
-
-/*
-function createWebsocketServer(): any {
-    const httpServer = http.createServer(function(request, response) {
-        console.log((new Date()) + ' Received request for ' + request.url);
-        response.writeHead(404);
-        response.end();
-    });
-    httpServer.listen(WS_SERVER_PORT, function() {
-        console.log((new Date()) + ' Server is listening on port 8080');
-    });
-
-    const wsServer = new WebSocketServer({
-        httpServer,
-        // You should not use autoAcceptConnections for production
-        // applications, as it defeats all standard cross-origin protection
-        // facilities built into the protocol and the browser.  You should
-        // *always* verify the connection's origin and decide whether or not
-        // to accept it.
-        autoAcceptConnections: false
-    });
-
-    function originIsAllowed(origin) {
-      // put logic here to detect whether the specified origin is allowed.
-      return true;
-    }
-
-    wsServer.on('request', function(request) {
-        if (!originIsAllowed(request.origin)) {
-          // Make sure we only accept requests from an allowed origin
-          request.reject();
-          console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-          return;
-        }
-
-        var connection = request.accept('echo-protocol', request.origin);
-        console.log((new Date()) + ' Connection accepted.');
-        connection.on('message', function(message) {
-            if (message.type === 'utf8') {
-                console.log('Received Message: ' + message.utf8Data);
-                connection.sendUTF(message.utf8Data);
-            }
-            else if (message.type === 'binary') {
-                console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-                connection.sendBytes(message.binaryData);
-            }
-        });
-        connection.on('close', function(reasonCode, description) {
-            console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-        });
-    });
-
-    return wsServer;
-}
-*/
-
-
